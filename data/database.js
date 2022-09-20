c109758acea35e9b972c4168b443863691ec6e85
itay <itailavi0212@gmail.com>
2022-09-20 23:16:47 +0300

    Changed Url

--- data/database.js
+++ data/database.js
@@ -3,6 +3,7 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let mongodbUrl = 'mongodb://localhost:27017';
+let srvMongodbUrl = 'mongodb+srv://WDE:Eb7514804@cluster0.bwa2nw4.mongodb.net/?retryWrites=true&w=majority';
 
 if (process.env.MONGODB_URL) {
 	mongodbUrl = process.env.MONGODB_URL;
@@ -11,7 +12,7 @@ if (process.env.MONGODB_URL) {
 let database;
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect(mongodbUrl);
+  const client = await MongoClient.connect(srvMongodbUrl);
   database = client.db('online-shop');
 }
 
