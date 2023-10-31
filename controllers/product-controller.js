085f387cc470e91543df526001e7568e93d99e0e
itay <itailavi0212@gmail.com>
2023-10-31 15:37:21 +0200

    add prices, fixed style issues

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,29 +1,22 @@
 const Product = require('../models/product-model');
 
-const slides = [
-  {
-      imageUrl: "https://res.cloudinary.com/dkvvhhtsq/image/upload/v1698679810/axwb2bm2cdunu79h2nqn.jpg",
-      alt: "Slide 1",
-      title: "DJI FPV Drone",
-      price: "$1299.99"
-  },
-  {
-      imageUrl: "https://res.cloudinary.com/dkvvhhtsq/image/upload/v1698750003/ovfkzjmg9ap1eb2jgzl3.jpg",
-      alt: "Slide 2",
-      title: "Ultimate 5-Inch Racer Drone",
-      price: "$250"
-  },
-  {
-      imageUrl: "https://res.cloudinary.com/dkvvhhtsq/image/upload/v1698680083/zgqwolnmjuzds6o01as3.jpg",
-      alt: "Slide 3",
-      title: "Tattu R-Line 1300mAh 120C LiPo",
-      price: "$30"
-  }
-];
 async function getAllProducts(req, res, next) {
-
-
   try {
+    const slides = [
+      {
+        ...(await Product.findById('653fca13845e7a429a009660')),
+        higherPrice: 1399.99,
+      },
+      {
+        ...(await Product.findById('6540de33edcced66ae3b343a')),
+        higherPrice: 300,
+      },
+      {
+        ...(await Product.findById('653fcd13845e7a429a009664')),
+        higherPrice: 37.5,
+      },
+    ];
+
     const products = await Product.findAll();
     res.render('customer/products/all-products', { products, slides });
   } catch (error) {
@@ -31,17 +24,16 @@ async function getAllProducts(req, res, next) {
   }
 }
 
-async function getProductDetails(req,res,next) {
-	try {
-		const product = await Product.findById(req.params.id);
-		res.render('customer/products/product-details', { product: product });
-	} catch(error) {
-		next(error)
-	}
-	
+async function getProductDetails(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render('customer/products/product-details', { product: product });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
   getAllProducts: getAllProducts,
-  getProductDetails: getProductDetails
+  getProductDetails: getProductDetails,
 };
